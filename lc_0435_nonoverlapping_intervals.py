5f64ff3f5e64a0667e80d46e30abfb976a282043
Adam Kunesh <apkunesh@gmail.com>
2025-08-27 10:14:29 -0700

    〰 nonoverlapping intervals

---
+++ lc_0435_nonoverlapping_intervals.py
@@ -0,0 +1,18 @@
+from heapq import heapify, heappop
+from typing import List
+
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        # This is gonna be nlogn anyway, could be better to just sort up front.
+        heapify(intervals)
+        left, delete_count = heappop(intervals), 0
+        while intervals:
+            candidate = heappop(intervals)
+            if candidate[0] >= left[1]:  # Happy case, they don't overlap
+                left = candidate
+                continue
+            # So, they do overlap. Then we take the one which has the smallest right frontier
+            left = left if candidate[1] > left[1] else candidate
+            delete_count += 1
+        return delete_count
