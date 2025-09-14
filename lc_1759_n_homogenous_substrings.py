db5a5f0510dba1caf2c485167a02b5efb770d3bc
Adam Kunesh <apkunesh@gmail.com>
2025-09-14 16:13:53 -0700

    Do it in 10 lines

--- lc_1759_n_homogenous_substrings.py
+++ lc_1759_n_homogenous_substrings.py
@@ -1,14 +1,10 @@
 class Solution:
     def countHomogenous(self, s: str) -> int:
-        count = 1
-        prev = s[0]
-        grand_max = 0
+        count, prev, grand_max = 1, s[0], 0
         for i in range(1, len(s)):
             if s[i] == prev:
                 count += 1
             else:
-                grand_max += count * (count + 1) // 2
-                prev = s[i]
-                count = 1
+                grand_max, prev, count = grand_max + count * (count + 1) // 2, s[i], 1
         grand_max += count * (count + 1) // 2
         return grand_max % (10**9 + 7)
