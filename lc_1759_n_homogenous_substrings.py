083a3ad5c935dea7723b039f71dc1f3f1b05b01f
Adam Kunesh <apkunesh@gmail.com>
2025-09-14 16:12:25 -0700

    😇 memory-efficient soln

--- lc_1759_n_homogenous_substrings.py
+++ lc_1759_n_homogenous_substrings.py
@@ -1,26 +1,14 @@
-from collections import defaultdict
-
-
 class Solution:
     def countHomogenous(self, s: str) -> int:
-        if len(s) == 1:
-            return 1  # just in case
-        contig_to_count = defaultdict(int)
-        counts = set()
-        n_letters, cur_letter = 1, s[0]
+        count = 1
+        prev = s[0]
+        grand_max = 0
         for i in range(1, len(s)):
-            if s[i] == cur_letter:
-                n_letters += 1
+            if s[i] == prev:
+                count += 1
             else:
-                counts.add(n_letters)
-                contig_to_count[n_letters] += 1
-                n_letters, cur_letter = 1, s[i]
-        counts.add(n_letters)
-        counts = list(counts)
-        counts.sort()
-        contig_to_count[n_letters] += 1
-        local_result, grand_result = 0, 0
-        for i in range(counts[-1] + 1):
-            local_result += i
-            grand_result += local_result * contig_to_count[i]
-        return grand_result % (10**9 + 7)
+                grand_max += count * (count + 1) // 2
+                prev = s[i]
+                count = 1
+        grand_max += count * (count + 1) // 2
+        return grand_max % (10**9 + 7)
