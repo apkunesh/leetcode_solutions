38890e35541f68623a616abc201005a74fe6271d
Adam Kunesh <apkunesh@gmail.com>
2025-10-04 15:13:21 -0700

    🪨 got exploded by merge stones yet again

--- lc_1000_min_cost_to_merge_stones.py
+++ lc_1000_min_cost_to_merge_stones.py
@@ -1,86 +1,77 @@
 from typing import List
 
 
-def update_partition(partition_in, k) -> List:
-    print(f"Updating {partition_in}")
-    partition_index = len(partition_in) - 1
-    count = 0
-    while partition_index > 0:
-        print("Part ind is ", partition_index)
-        left_max = len(partition_in) - 1 - count
-        tent = partition_in[partition_index] + k - 1
-        if tent > left_max:
-            count += 1
-            partition_index -= 1
-            continue
-        count = 0
-        for i in range(partition_index, len(partition_in)):
-            partition_in[i] = tent + count
-        print(f"to {partition_in}")
-        return partition_in
-    return []
-
-
 class Solution:
     def mergeStones(self, stones: List[int], k: int) -> int:
-        if k > len(stones):
-            print("Wrong lenghts")
-            return -1
-        elif k == len(stones):
-            return sum(stones)
+        # Note that with 4, 2, 4->3->2->1
+        # While with 4,3, 4->2->XX
+        # and with 4,3 5->3->1 xx
+        # With 8,4, 8 -> 5 -> 2 XX
+        # Mean while 10,4 gives 10->7->4->1
+        # -> (len(stones)-1)%k==0 or else we return -1.
         if (len(stones) - 1) % (k - 1) != 0:
-            print("Too samll")
             return -1
-        # Otherwise it's possible.
-        # Let's try to just build this bad boy up from permutations
-        sums = {}
-        # O(n**2)
-        for i in range(len(stones)):
-            cur_sum = 0
-            for j in range(i, len(stones)):
-                cur_sum += stones[j]
-                sums[(i, j)] = cur_sum
-        min_costs = {(i, i): 0 for i in range(len(stones))}
-        :"!"
+        stones = [0] + stones + [0]
+        # Now we consider L and R outer stones as the "last to be merged" and find the min inside. Note that the dummy stones find us a minimum score.
+        partial_sum = [0]
+        for i in range(1, len(stones)):
+            partial_sum.append(partial_sum[-1] + stones[i])
+            # Value between 0 and 2 is then partial_sum[2-1] - partial_sum[0
 
-        def recurse(i, j, level,partition_here):  # We can assume that the i,j here can be cleanly done
-            print(f"LEVEL {level}")
-            if (i, j) in min_costs:
-                print(f"Cache hit at {i,j}: ", min_costs[(i, j)])
-                return min_costs[(i, j)] + sums[(i, j)]
-            partition_lefts = [i + elem for elem in range(k)]
-            local_min = 10**30
-            while partition_lefts != []:
-                print(f"part lefts: ", partition_lefts)
-                part_rights = [elem - 1 for elem in partition_lefts][1:] + [j]
-                local_sum = 0
-                for left, right in zip(partition_lefts, part_rights):
-                    print("Recursing with ", [left, right])
-                    local_sum += recurse(left, right, level + 1,partition_lefts)
-                local_min = min(local_min, local_sum)
-                # Now we update the partition
-                partition_lefts = update_partition(partition_lefts, k)
-            print(f"Cache create at {i},{j}: ", local_min)
-            min_costs[(i, j)] = local_min
-            return min_costs[(i, j)] + sums[(i, j)]
+        cost_cache = {}
 
-        result = recurse(0, len(stones) - 1, 0)
-        print(min_costs)
-        return result
+        def recurse(i, j, n_piles):
+            if j - i + 1 == n_piles:
+                print(
+                    f"Exact size found with start at {i} and end at {j} with {n_piles} piles"
+                )
+                cost_cache[(i, j, k)] = 0
+                return cost_cache[(i, j, k)]
+            if j - i < k - 1:
+                print(
+                    f"Distance between {i} and {j} too small for {k} elements; returning inf"
+                )
+                return float("infinity")
+            else:
+                big_total_cost = float("infinity")
+                for partition in range(i + 1, j):
+                    for left_piles in range(1, k):
+                        right_piles = k - left_piles
+                        print(
+                            f"Attempting to split from {i} to {j} at index {partition} into {left_piles} left and {right_piles} right piles"
+                        )
+                        left_cost = recurse(i, partition - 1, left_piles)
+                        right_cost = recurse(partition, j, right_piles)
+                        print(f"Left and right costs are {left_cost} and {right_cost}")
+                        big_total_cost = min(
+                            big_total_cost,
+                            left_cost
+                            + right_cost
+                            + partial_sum[j - 1]
+                            - partial_sum[i],
+                        )
+                cost_cache[(i, j, k)] = big_total_cost
+                return cost_cache[(i, j, k)]
+
+        # Basically I need "what's the cost to condense the stuf inside into 2 piles"
+        recurse(0, len(stones), 1)
+        print(cost_cache)
+        return cost_cache[(1, len(stones) - 1)]
 
 
-# Constraints:
-#
-#     n == stones.length
-#     1 <= n <= 30
-#     1 <= stones[i] <= 100
-#     2 <= k <= 30
+print(f"{Solution().mergeStones([3,2,4,1],2)} should be 20")
+print(f"{Solution().mergeStones([3,2,4,1],3)} should be -1")
+print(f"{Solution().mergeStones([3,5,2,1,6],3)} should be 20")
+
+"""
+Input: stones = [3,2,4,1], k = 2
+Output: 20
+
+Input: stones = [3,2,4,1], k = 3
+Output: -1
 
 
-soln = Solution()
-res1 = soln.mergeStones([3, 2, 4, 1], 2)
-assert res1 == 20, res1
-res2 = soln.mergeStones([3, 2, 4, 1], 3)
-assert res2 == -1, res2
-res3 = soln.mergeStones([3, 5, 1, 2, 6], 3)
-assert res3 == 25, res3
+Input: stones = [3,5,1,2,6], k = 3
+Output: 25
+
+"""
