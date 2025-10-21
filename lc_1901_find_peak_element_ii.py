fb9ca550ecb912927379fc002b91446c6acb397c
Adam Kunesh <apkunesh@gmail.com>
2025-10-21 11:03:25 -0700

    💠 nutty lattice / grid max problem

---
+++ lc_1901_find_peak_element_ii.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
+        c_c = len(mat)//2
+        side = None
+        found = False
+        while not found:
+            
+        return [0, 0]
